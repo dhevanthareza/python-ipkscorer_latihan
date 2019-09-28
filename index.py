303f865d40dce5c1b936f7b3b95bcf70ba2435e3
dhevanthareza <dhevanmuhamad@gmail.com>
2019-09-28 22:44:23 +0700

    inputan angka

--- index.py
+++ index.py
@@ -1,20 +1,27 @@
-def score(val):
+def konverter(val):
 	if 4 >= val > 3.5: return 'A' 
 	elif 3.5 >= val > 3: return 'AB'
-	elif val == 'b': return 'B'
-	elif val == 'bc': return 'BC'
-	# elif val == 'c': return 2.0
-	# elif val == 'd': return 1.0
-	# elif val == 'e': return 0
-kalkulus = score(float(input("Masukan nilai Kalkulus : ")))
-print(kalkulus)
-# fisika = score(input("Masukan nilai fisika : "))
-# daspro = score(input("Masukan nilai Dasar Pemrograman : "))
-# daskom = score(input("Masukan nilai Dasar Komputer : "))
-# bing = score(input("Masukan nilai Bahasa Inggris : "))
-# agama = score(input("Masukan nilai Pendidikan Agama : "))
-# pti = score(input("Masukan nilai Pengantar Teknologi Informasi : "))
-# hitung = (kalkulus * 4 + fisika * 4 + daspro * 4 + daskom * 2 + bing * 2 + agama * 2 + pti * 2)/20
-# print("=========================================================")
+	elif 3 >= val > 2.5: return 'B'
+	elif 2.5 >= val > 2: return 'BC'
+	elif 2 >= val > 1.5: return 'C'
+	elif 1.5 >= val > 1: return 'D'
+	elif 1 >= val: return "E"
+kalkulus = float(input("Masukan nilai Kalkulus : "))
+fisika = float(input("Masukan nilai fisika : "))
+daspro = float(input("Masukan nilai Dasar Pemrograman : "))
+daskom = float(input("Masukan nilai Dasar Komputer : "))
+bing = float(input("Masukan nilai Bahasa Inggris : "))
+agama = float(input("Masukan nilai Pendidikan Agama : "))
+pti = float(input("Masukan nilai Pengantar Teknologi Informasi : "))
+total = (kalkulus * 4 + fisika * 4 + daspro * 4 + daskom * 2 + bing * 2 + agama * 2 + pti * 2)/20
+print("\n=========================================================\n")
 
-# print("IPK KAMU ADALAH : ", hitung)
+print("Nilai Kalkulus\t\t: {} ({})".format(kalkulus, konverter(kalkulus)))
+print("Nilai fisika		: {} ({})".format(fisika, konverter(fisika)))
+print("Nilai daspro		: {} ({})".format(daspro, konverter(daspro)))
+print("Nilai daskom		: {} ({})".format(daskom, konverter(daskom)))
+print("Nilai bing		: {} ({})".format(bing, konverter(bing)))
+print("Nilai agama		: {} ({})".format(agama, konverter(agama)))
+print("Nilai pti		: {} ({})".format(pti, konverter(pti)))
+print("===========================")
+print("Nilai IPK		: {} ({})".format(round(total, 2), konverter(total)))
